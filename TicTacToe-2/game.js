557d3cb148f26571de6ca8bda5f33b9f2719a7c2
Ahfreakin <vladmilner99@gmail.com>
2023-07-22 18:10:16 -0400

    Game Finished

--- TicTacToe-2/game.js
+++ TicTacToe-2/game.js
@@ -2,27 +2,35 @@ const readLine = require('./lib/readLine');
 const clear = require('./lib/clear');
 
 const game = async () => {
-  let p1 = '-';
-  let p2 = '-';
-  let p3 = '-';
-  let p4 = '-';
-  let p5 = '-';
-  let p6 = '-';
-  let p7 = '-';
-  let p8 = '-';
-  let p9 = '-';
+  // let p1 = '-';
+  // let p2 = '-';
+  // let p3 = '-';
+  // let p4 = '-';
+  // let p5 = '-';
+  // let p6 = '-';
+  // let p7 = '-';
+  // let p8 = '-';
+  // let p9 = '-';
+  let positions = Array(9).fill('-');
+ // let myarray = [9];
   let playerOne = 'X'
   let playerTwo = 'O'
   let currentPlayer = playerOne;
   let errorMessage = '';
   let gameOver = false;
   let gameOverTie = false;
+  let winningPlayer;
+  // let gameOverWin = false;
+  let winningCombinations = [
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
+  
   while (gameOver === false) {
     clear();
-    console.log(`[ ${p1} , ${p2} , ${p3} ]`);
-    console.log(`[ ${p4} , ${p5} , ${p6} ]`);
-    console.log(`[ ${p7} , ${p8} , ${p9} ]`);
+    for (let row = 0; row <= 2; row++) {
+    console.log(`[ ${positions[row*3 + 0]} , ${positions[row*3 + 1]} , ${positions[row*3 + 2]} ]`);
+    //console.log(`${row} [ ${row*3 + 0} , ${row*3 + 1} , ${row*3 + 2} ]`);
+    }
     console.log(`it is ${currentPlayer}'s turn`);
 
     if (errorMessage.length > 0){
@@ -34,124 +42,56 @@ const game = async () => {
 
     let positionFilled = false;
 
-    if (turn === '1') {
-      if (p1 === '-') {
-        p1 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '2') {
-      if (p2 === '-') {
-        p2 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '3') {
-      if (p3 === '-') {
-        p3 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '4') {
-      if (p4 === '-') {
-        p4 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '5') {
-      if (p5 === '-') {
-        p5 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '6') {
-      if (p6 === '-') {
-        p6 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '7') {
-      if (p7 === '-') {
-        p7 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '8') {
-      if (p8 === '-') {
-        p8 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
-    }
-    else if (turn === '9') {
-      if (p9 === '-') {
-        p9 = currentPlayer;
-        positionFilled = true;
-      } else {
-        errorMessage = 'This position is already filled.';
-      }
+    if ( turn !== '1' && turn !== '2' && turn !== '3' && turn !== '4' && turn !== '5' && turn !== '6' && turn !== '7' && turn !== '8' && turn !== '9') {
+      errorMessage = 'Invalid input';
+      continue;
+    } 
+    if (positions[turn - 1] !== '-') {
+      errorMessage = 'This position is already filled';
+      continue;
+    } 
+    // fill board with X or O
+    positions[turn - 1] = currentPlayer;
 
-    } else {
-     errorMessage = 'Invalid Input'
-    }
 
-    if (positionFilled === true) {
-      if (currentPlayer === playerOne) {
-        currentPlayer = playerTwo;
-      } else {
-        currentPlayer = playerOne;
+    // implement logic of win
+     for (let i = 0; i < 8; i++) {
+      let wc = winningCombinations[i];
+      // i =1 , wc = [3,4,5]
+      if (positions[wc[0]] !== '-' && positions[wc[0]] === positions[wc[1]] && positions[wc[1]] === positions[wc[2]]) {
+        gameOver = true;
+        // gameOverWin = true;
+        winningPlayer = currentPlayer;
+        continue;
       }
-    }
-
-    if (p1 === p2 && p2 === p3 && p1 === playerOne) {
-      gameOver = true;
-      currentPlayer = playerOne;
-    } else if (p1 === p2 && p2 === p3 && p1 === playerTwo) {
-      gameOver = true;
+     }
+      // switch player
+    if (currentPlayer === playerOne) {
       currentPlayer = playerTwo;
-    } else if (p4 === p5 && p5 === p6 && p4 === playerOne) {
-      gameOver = true;
-      currentPlayer = playerOne;
-    } else if (p4 === p5 && p5 === p6 && p4 === playerTwo) {
-      gameOver = true;
-      currentPlayer = playerTwo;
-   } else {
-    if (p1 === '-'|| p2 === '-'|| p3 === '-'|| p4 === '-'|| p5 === '-'|| p6 === '-'|| p7 === '-'|| p8 === '-'|| p9 === '-') {
-      gameOver = false;
     } else {
+      currentPlayer = playerOne;
+    }
+     // check if all cells are filled
+     let freeSpace = false;
+     for (let i = 0; i < 9; i++) {
+      if (positions[i] === '-') {
+        freeSpace = true;
+        break;
+      }
+     }
+     if (freeSpace === false) {
       gameOver = true;
       gameOverTie = true;
-    }
-   }
+     }
   }
-   
+  clear();
+  for (let row = 0; row <= 2; row++) {
+    console.log(`[ ${positions[row*3 + 0]} , ${positions[row*3 + 1]} , ${positions[row*3 + 2]} ]`);
+    }
   if (gameOverTie === true) {
-    clear();
-      console.log(`[ ${p1} , ${p2} , ${p3} ]`);
-      console.log(`[ ${p4} , ${p5} , ${p6} ]`);
-      console.log(`[ ${p7} , ${p8} , ${p9} ]`);
     console.log('This game has ended in a draw.')
    } else {
-    clear();
-    console.log(`[ ${p1} , ${p2} , ${p3} ]`);
-    console.log(`[ ${p4} , ${p5} , ${p6} ]`);
-    console.log(`[ ${p7} , ${p8} , ${p9} ]`);
-  console.log(`Player ${currentPlayer} wins.`)
+    console.log(`Player ${winningPlayer} wins.`)
    }
 };
 
